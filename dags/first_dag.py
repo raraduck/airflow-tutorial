cbe2dd1836ffcde8ae57d1d13f7a8023bf11f698
dwnusa-win10 <dwnusa@gmail.com>
2025-09-18 19:55:20 +0900

    update

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -5,6 +5,7 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 import os, shutil
 import findspark
+import argparse
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64/jre"
 os.environ["SPARK_HOME"] = "/var/lib/airflow/spark/spark-3.4.1-bin-hadoop3"
 os.environ["PATH"]       = f'{os.environ["SPARK_HOME"]}/bin:{os.environ["SPARK_HOME"]}/sbin:' + os.environ["PATH"]
@@ -26,11 +27,14 @@ with DAG(
     def python_print():
         print("python")
 
-    def check_spark():
+    def check_spark(access_key: str, secret_key: str):
         from pyspark.sql import SparkSession
         spark = SparkSession.builder \
             .appName("first-spark-app") \
             .master("local[*]") \
+            .config("spark.hadoop.fs.s3a.access.key", access_key) \
+            .config("spark.hadoop.fs.s3a.secret.key", secret_key) \
+            .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
             .getOrCreate()
 
         # print(spark)
@@ -60,4 +64,9 @@ with DAG(
 # init main 부분 추가
 # =====================
 if __name__ == "__main__":
-    check_spark()
+    parser = argparse.ArgumentParser(description="Run Spark job with AWS credentials")
+    parser.add_argument("--access-key", required=True, help="AWS Access Key")
+    parser.add_argument("--secret-key", required=True, help="AWS Secret Key")
+
+    args = parser.parse_args()
+    check_spark(args.access_key, args.secret_key)
