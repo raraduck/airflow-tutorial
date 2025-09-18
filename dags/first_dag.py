b94ea92a6380d43334d1b669e5977cda7849d423
dwnusa-win10 <dwnusa@gmail.com>
2025-09-18 15:57:14 +0900

    update

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -3,8 +3,8 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
-from pyspark.sql import SparkSession
-import os, shutil, pyspark
+# from pyspark.sql import SparkSession
+import os, shutil
 
 with DAG(
     dag_id="my_first_spark_dag",
@@ -22,22 +22,22 @@ with DAG(
     def python_print():
         print("python")
 
-    def check_spark():
-        os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64/jre"
-        os.environ["SPARK_HOME"] = "/var/lib/airflow/spark/spark-3.4.1-bin-hadoop3"
-        os.environ["PATH"]       = f'{os.environ["SPARK_HOME"]}/bin:{os.environ["SPARK_HOME"]}/sbin:' + os.environ["PATH"]
-        spark = SparkSession.builder \
-            .appName("first-spark-app") \
-            .master("local[*]") \
-            .getOrCreate()
+    # def check_spark():
+    #     os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64/jre"
+    #     os.environ["SPARK_HOME"] = "/var/lib/airflow/spark/spark-3.4.1-bin-hadoop3"
+    #     os.environ["PATH"]       = f'{os.environ["SPARK_HOME"]}/bin:{os.environ["SPARK_HOME"]}/sbin:' + os.environ["PATH"]
+    #     spark = SparkSession.builder \
+    #         .appName("first-spark-app") \
+    #         .master("local[*]") \
+    #         .getOrCreate()
 
-        print(spark)
-        df = spark.range(10)
-        df.show()
+    #     print(spark)
+    #     df = spark.range(10)
+    #     df.show()
 
     python_task = PythonOperator(
         task_id='python_task',
-        python_callable=check_spark
+        python_callable=python_print
     )
 
     end = DummyOperator(task_id="end")
