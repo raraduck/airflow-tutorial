6416cccdaed95dca1c64b92cb9408d9849981628
dwnusa-win10 <dwnusa@gmail.com>
2025-09-18 23:18:11 +0900

    update

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -20,6 +20,26 @@ os.environ["SPARK_HOME"] = os.getenv("SPARK_HOME", "/var/lib/airflow/spark/spark
 os.environ["PATH"] = f'{os.environ["SPARK_HOME"]}/bin:{os.environ["SPARK_HOME"]}/sbin:' + os.environ["PATH"]
 findspark.init(os.environ["SPARK_HOME"])
 
+def python_print():
+    print("python")
+
+def check_spark(access_key: str=None, secret_key: str=None):
+    from pyspark.sql import SparkSession
+    spark = SparkSession.builder \
+        .appName("first-spark-app") \
+        .master("local[*]") \
+        .config("spark.jars", "/var/lib/airflow/spark/jars/hadoop-aws-3.3.4.jar,/var/lib/airflow/spark/jars/aws-java-sdk-bundle-1.12.262.jar") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID")) \
+        .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY")) \
+        .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
+        .getOrCreate()
+
+    df = spark.read.text("s3a://databricks-workspace-stack-60801-bucket/users_orders/users.csv")
+
+    # 처음 몇 줄 출력
+    df.show(10, truncate=False)
+    spark.stop()
 
 with DAG(
     dag_id="my_first_spark_dag",
@@ -35,31 +55,6 @@ with DAG(
         bash_command='echo "bash"'
     )
 
-    def python_print():
-        print("python")
-
-    def check_spark(access_key: str=None, secret_key: str=None):
-        from pyspark.sql import SparkSession
-        spark = SparkSession.builder \
-            .appName("first-spark-app") \
-            .master("local[*]") \
-            .config("spark.jars", "/var/lib/airflow/spark/jars/hadoop-aws-3.3.4.jar,/var/lib/airflow/spark/jars/aws-java-sdk-bundle-1.12.262.jar") \
-            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-            .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID")) \
-            .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY")) \
-            .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
-            .getOrCreate()
-
-        # print(spark)
-        # df = spark.range(10)
-        # df.show()
-
-        df = spark.read.text("s3a://databricks-workspace-stack-60801-bucket/users_orders/users.csv")
-
-        # 처음 몇 줄 출력
-        df.show(10, truncate=False)
-        spark.stop()
-
     python_task = PythonOperator(
         task_id='python_task',
         python_callable=python_print
@@ -70,23 +65,22 @@ with DAG(
         python_callable=check_spark
     )
 
-    run_spark_etl = SparkSubmitOperator(
-        task_id="run_user_order_etl",
-        application="/var/lib/airflow/spark/check_spark.py",  # 위 스크립트 경로
-        conn_id="spark_default",                          # Airflow에 설정된 Spark 연결 ID
-        verbose=True,
-        application_args=[
-            "--access-key",  os.getenv("AWS_ACCESS_KEY_ID"),
-            "--secret-key",  os.getenv("AWS_SECRET_ACCESS_KEY"),
-            # "--users",  "s3a://databricks-workspace-stack-60801-bucket/users_orders/users.csv",
-            # "--orders", "s3a://databricks-workspace-stack-60801-bucket/users_orders/orders.json"
-        ],
-    )
-
+    # run_spark_etl = SparkSubmitOperator(
+    #     task_id="run_user_order_etl",
+    #     application="/var/lib/airflow/spark/check_spark.py",  # 위 스크립트 경로
+    #     conn_id="spark_default",                          # Airflow에 설정된 Spark 연결 ID
+    #     verbose=True,
+    #     application_args=[
+    #         "--access-key",  os.getenv("AWS_ACCESS_KEY_ID"),
+    #         "--secret-key",  os.getenv("AWS_SECRET_ACCESS_KEY"),
+    #         # "--users",  "s3a://databricks-workspace-stack-60801-bucket/users_orders/users.csv",
+    #         # "--orders", "s3a://databricks-workspace-stack-60801-bucket/users_orders/orders.json"
+    #     ],
+    # )
 
     end = DummyOperator(task_id="end")
 
-    start >> bash_task >> python_task >> spark_task >> run_spark_etl >> end
+    start >> bash_task >> python_task >> spark_task >> end
 
 # =====================
 # init main 부분 추가
