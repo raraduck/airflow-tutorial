85530e0e844971735ee497fda03ed4f3795beb2e
dwnusa-win10 <dwnusa@gmail.com>
2025-09-20 15:16:21 +0900

    update: databricks with airflow connection

--- dags/databricks_dag.py
+++ dags/databricks_dag.py
@@ -6,10 +6,11 @@ default_args = {
   'owner': 'airflow'
 }
 
-with DAG('databricks_dag',
-  start_date = days_ago(2),
-  schedule_interval = None,
-  default_args = default_args
+with DAG(
+    'databricks_dag',
+    start_date = days_ago(2),
+    schedule_interval = None,
+    default_args = default_args
   ) as dag:
 
   opr_run_now = DatabricksRunNowOperator(
